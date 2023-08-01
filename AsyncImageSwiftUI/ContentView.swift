49b0248b191bcf3a91ea9e24f64759697f75c83c
Zeynep Bayrak Demirel <zeynepbayrak863@gmail.com>
2023-08-01 11:24:51 +0300

    last commit

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        
+        NavigationView {
+            
+            VStack{
+                
+                AsyncImage(url: URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjbxeu1PXDKUAMT1MHlWMKDcfnntkf0gD4oU1yojxje79y1kt0_dsTL8PmPBoVjDnSueE&usqp=CAU")!) { image in
+                    image.resizable().frame(width: 300, height: 300, alignment: .center)
+                } placeholder: {
+                    ProgressView() // dönen işaret görünümü, inene kadar
+                }
+
+            
+             List(superHeroArray) { Superhero in
+                 Text(Superhero.name)
+                .font(.title3)
+                .foregroundColor(.blue)
+            
+        }
+        }.navigationTitle(Text("Superhero Book"))
+        }
 }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
