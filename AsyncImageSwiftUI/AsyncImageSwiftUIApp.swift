45041de325f62ded6c849ab9732414246bca0efb
Zeynep Bayrak Demirel <zeynepbayrak863@gmail.com>
2023-08-01 10:57:48 +0300

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Zeynep Bayrak Demirel on 1.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
